cd8f747681573e19e62f4e4afbcdb73c4b50a299
Vivek <tailorvivekgec19@gmail.com>
2023-03-20 15:12:04 +0530

    resume builder using flutter framwork and dart language

--- lib/resumeDataModal.dart
+++ lib/resumeDataModal.dart
@@ -8,28 +8,3 @@ class ResumeModal
     this.u_project_details,this.u_training_place,this.u_training_year,this.u_tech_skills,this.u_soft_skills,
     this.u_ref,this.u_refcompany,this.u_refjob,this.u_refemail});
 }
-
-/*
-
-
-  TextEditingController u_name = TextEditingController();
-  TextEditingController u_address = TextEditingController();
-  TextEditingController u_email = TextEditingController();
-  TextEditingController u_contact = TextEditingController();
-  List edu = ["B.E.", "M.B.B.S", "Ph.D", "SSC", "HSC", "B.Sc"];
-  String? eduSel;
-  List techSkill = [ "Basic Programming", "Web Designing", "Network Security",  "Mobile Developing","Database"]
-  List techselect = [false, false, false, false, false];
-  List softSkill = [ "Public Speaking","LeaderShip","Time Management","Critical Thinking","Positive attitude"];
-  List softselect = [false, false, false, false, false];
-  bool project = false;
-  bool demo = false;
-  bool mode = false;
-  Color light = Colors.brown.shade500;
-  Color dark = Colors.black;
-  Color c1 = Colors.brown.shade500;
-  RangeValues rangeValues = RangeValues(25000, 250000);
-  String country = "Indian";
-  String? countrySel;
-
-* */
