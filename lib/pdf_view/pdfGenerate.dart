cfadadcff791e2515b857f05ed5a60a5f3c23c3d
VIVEK <tailorvivekgec19@gmail.com>
2023-03-23 10:01:37 +0530

    pdf

--- lib/pdf_view/pdfGenerate.dart
+++ lib/pdf_view/pdfGenerate.dart
@@ -1,27 +1,27 @@
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-import 'package:path_provider_windows/path_provider_windows.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
-
-Future<void> createPDF()
-async {
-  var pdf = pw.Document();
-  pdf.addPage(
-    pw.Page(
-      margin: pw.EdgeInsets.all(15),
-      pageFormat: PdfPageFormat.a4,
-      orientation: pw.PageOrientation.portrait,
-      build: (context) {
-        return pw.Center(
-          child: pw.Container(color: PdfColors.red,height: 150,width: 150)
-        );
-      },
-    )
-  );
-
-  Directory? directory = await getApplicationSupportDirectory();
-  File file = File("${directory!.path}/flutter.pdf");
-  print("${directory!.path}/flutter.pdf");
-  await file.writeAsBytes(await pdf.save());
-}
+// import 'dart:io';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:path_provider_windows/path_provider_windows.dart';
+// import 'package:pdf/pdf.dart';
+// import 'package:pdf/widgets.dart' as pw;
+//
+// Future<void> createPDF()
+// async {
+//   var pdf = pw.Document();
+//   pdf.addPage(
+//     pw.Page(
+//       margin: pw.EdgeInsets.all(15),
+//       pageFormat: PdfPageFormat.a4,
+//       orientation: pw.PageOrientation.portrait,
+//       build: (context) {
+//         return pw.Center(
+//           child: pw.Container(color: PdfColors.red,height: 150,width: 150)
+//         );
+//       },
+//     )
+//   );
+//
+//   Directory? directory = await getExternalStorageDirectory();
+//   File file = File("${directory!.path}/flutter.pdf");
+//   print("${directory!.path}/flutter.pdf");
+//   await file.writeAsBytes(await pdf.save());
+// }
